ab6b2caa28fd92924c631f636838f71344df72a8
cuppajoeman <ccn@cuppajoeman.com>
2025-02-05 03:58:37 -0500

    feat: add packet data

---
+++ packets.hpp
@@ -0,0 +1,19 @@
+#ifndef PACKETS_HPP
+#define PACKETS_HPP
+
+#include <cstdint>
+#include <vector>
+
+#include "sbpt_generated_includes.hpp"
+
+struct PacketHeader {
+    PacketType type;
+    uint32_t size_of_data_without_header;
+};
+
+struct PacketWithSize {
+    std::vector<char> data; // Use std::vector<char> to hold packet data
+    size_t size;            // Size of the packet data 1 unit equals 1 byte = 8 bits
+};
+
+#endif
